c6e6256bf1b9b2f1033b1a132c1a7d5f7b2c0842
cedarwarman <cedardalewarman@gmail.com>
2023-11-02 13:53:22 -0700

    Start working on R project.

---
+++ R/make_kgwasflow_config_files.R
@@ -0,0 +1,52 @@
+# Introduction ------------------------------------------------------------
+# This script builds config files for kGWASflow.
+
+library(dplyr)
+library(googlesheets4)
+library(tidyr)
+
+# Adding my Google service account credentials
+gs4_auth(path = "~/.credentials/google_sheets_api/service_account.json")
+
+# Test run: Varitome and flower measurements ------------------------------
+# Here I will test kGWASflow with the anther / pistil ratio measurements and 
+# the Varitome accessions. I'd like to know how the pipeline functions and how 
+# it scales.
+flower_phenotypes <- read_sheet("1Jyv0pj_SpE_9evfOt0Eb2nGFJN_Kfg4kMC3l1sJLsJU") %>%
+  group_by(accession_id) %>%
+  summarize(mean_anther_pistil_ratio = mean(anther_over_pistil)) %>%
+  rename(accession = accession_id)
+
+varitome_metadata <- read.table(
+    file = file.path(getwd(), "R_data", "varitome_metadata.txt"),
+    sep = ',',
+    header = TRUE
+  ) %>%
+  select(Run, Sample.Name)
+
+cedar_metadata <- read_sheet("1V2kH8G4tfYsYqnYb6bVHpGjwwkjd9le0arGBJ2o4r8s") %>%
+  select(name_CW, name_original_razifard)
+
+# Joining in the CW identifiers
+varitome_metadata <- varitome_metadata %>%
+  left_join(cedar_metadata, join_by(Sample.Name == name_original_razifard)) %>%
+  drop_na() %>%
+  rename(accession = name_CW)
+
+# There are multiple libraries for some of the accessions, figure out how to deal 
+# with these. I'll choose a random one for the test.
+qc <- varitome_metadata %>%
+  group_by(accession) %>%
+  summarize(n = n())
+
+set.seed(13)
+varitome_metadata <- varitome_metadata %>%
+  group_by(accession) %>%
+  slice_sample(n = 1)
+
+# Joining with the phenotype data
+samplesheet_flowers <- flower_phenotypes %>%
+  left_join(varitome_metadata, by = "accession") %>%
+  drop_na()
+
+# Now I need to get it in the right format, including the full fastq paths.
